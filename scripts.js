55129eae583bb0dcba226871eef828e006e6a764
Rafael pereira de oliveira <101524748+oliveira-dos-anjos@users.noreply.github.com>
2022-05-13 23:05:11 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,7 @@
+var Hamburger = document.querySelector(".Hamburger");
+
+Hamburger.addEventListener("click", function(){
+    document.querySelector(".container").classList.toggle("show-menu");
+});
+
+
