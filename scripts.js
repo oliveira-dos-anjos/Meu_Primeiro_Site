d1c1c4dbfafbe4e8d53532b10420edd15a3fc6a0
oliveira-dos-anjos <raffasadol@live.com>
2022-05-15 23:28:05 -0300

    adicionado recursos de cálculos para o orçamento

--- scripts.js
+++ scripts.js
@@ -4,4 +4,41 @@ Hamburger.addEventListener("click", function(){
     document.querySelector(".container").classList.toggle("show-menu");
 });
 
+document.querySelector("#qtde").addEventListener("change", atualizaPreco)
+document.querySelector("#JS").addEventListener("change", atualizaPreco)
+document.querySelector("#layout-sim").addEventListener("change", atualizaPreco)
+document.querySelector("#layout-nao").addEventListener("change", atualizaPreco)
+document.querySelector("#prazo").addEventListener("change", function () {
+    const prazo = document.querySelector("#prazo").value
+    document.querySelector("label[for=prazo]").innerHTML = `Prazo: ${prazo} semanas`
+    atualizaPreco()
+})
+
+
+function atualizaPreco(){
+    const qtde = document.querySelector("#qtde").value
+    const temJS = document.querySelector("#JS").checked
+    const incluiLayout = document.querySelector("#layout-sim").checked
+    const naoincluiLayout = document.querySelector("#layout-nao").checked
+    const prazo = document.querySelector("#prazo").value
+    
+    let preco = qtde * 50;
+    if (temJS) {
+        preco *= 1.1
+    }
+    if (incluiLayout) {
+        preco += 200
+    }
+
+    if (naoincluiLayout){
+        preco = preco
+    }
+
+    let taxaUrgencia = 1 - prazo*0.1;
+    preco *= 1 + taxaUrgencia
+
+    document.querySelector("#preco").innerHTML = 'R$' + preco.toFixed(2)
+
+}
+
 
