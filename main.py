e3bbeb85626a0542452d6a1fe948041b16d8a96e
Rafael pereira de oliveira <101524748+oliveira-dos-anjos@users.noreply.github.com>
2024-03-25 22:18:26 -0300

    Update main.py

--- main.py
+++ main.py
@@ -32,8 +32,8 @@ conn.close()  # Fechar
 # Configuração para Flask-Mail
 app.config['MAIL_SERVER'] = 'sandbox.smtp.mailtrap.io'
 app.config['MAIL_PORT'] = 587  # Use a porta adequada (25, 465, 587, ou 2525)
-app.config['MAIL_USERNAME'] = '941248ec6043d1'
-app.config['MAIL_PASSWORD'] = '********aab6'
+app.config['MAIL_USERNAME'] = 'A09F28282721CF'
+app.config['MAIL_PASSWORD'] = '3248403a8ce0e8'
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USE_SSL'] = False
 app.config['MAIL_DEFAULT_SENDER'] = 'raffasadol@gmail.com'
